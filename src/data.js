355ef7b4666e536e2f2d2fce8fa7eb2b6f8b43dd
Anishp10 <65644166+Anishp10@users.noreply.github.com>
2021-10-17 14:29:14 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,45 @@
+const data = [
+    {
+      id: 1,
+      email: 'ron@gmail.com',
+      first_name: 'Ron',
+      last_name: 'liam',
+      avatar: 'https://source.unsplash.com/JQ2D4I-2eyw/900x900',
+    },
+    {
+      id: 2,
+      email: 'Georgiano@gmail.com',
+      first_name: 'Georgiano',
+      last_name: 'Kross',
+      avatar: 'https://source.unsplash.com/LaK153ghdig/900x900',
+    },
+    {
+      id: 3,
+      email: 'ney@gmail.com',
+      first_name: 'Ney',
+      last_name: 'chandler',
+      avatar: 'https://source.unsplash.com/7uoMmzPd2JA/900x900',
+    },
+    {
+      id: 4,
+      email: 'joeeeben7@gmail.com',
+      first_name: 'Joey',
+      last_name: 'Benn',
+      avatar: 'https://source.unsplash.com/mXz64B8-3h0/900x900',
+    },
+    {
+      id: 5,
+      email: 'RossWilliam@gmail.com',
+      first_name: 'Ross',
+      last_name: 'williamson',
+      avatar: 'https://source.unsplash.com/BbSBf5uv50A/900x900',
+    },
+    {
+      id: 6,
+      email: 'orianasabatini@gmail.com',
+      first_name: 'Oriana',
+      last_name: 'Sabatina',
+      avatar: 'https://source.unsplash.com/JBwcenOuRCg/900x900',
+    },
+  ];
+  export default data;
